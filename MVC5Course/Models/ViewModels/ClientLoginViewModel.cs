a3fdf1dda7965f705ab7a5622ca6eff8080ba37c
ediux <ediuxcity@gmail.com>
2016-10-15 16:38:19 +0800

    [變更] 16 建立 ClientLoginViewModel 並製作客戶登入頁面與登入結果頁面

---
+++ MVC5Course/Models/ViewModels/ClientLoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ClientLoginViewModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "{0} 最大只能輸入 {1} 個字")]
+        [DisplayName("名")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("中間名")]
+        [StringLength(10, ErrorMessage = "Middle Name 最大只能輸入10個字")]
+        public string MiddleName { get; set; }
+        [Required]
+        [DisplayName("姓")]
+        [StringLength(5, ErrorMessage = "Last Name 最大只能輸入10個字")]
+        public string LastName { get; set; }
+        [Required]
+        [DisplayName("性別")]
+        [RegularExpression("[MF]", ErrorMessage = "只能輸入M或F!")]
+        public string Gender { get; set; }
+        [DisplayName("生日")]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+    }
+}
