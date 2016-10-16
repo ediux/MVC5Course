8d4ecb834ed612ef213d2b38ac3ba689fc129762
ediux <ediuxcity@gmail.com>
2016-10-16 11:45:11 +0800

    [變更] 18 練習透過 Entity Framework 更新資料與批次更新多筆資料

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,74 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+            var data = db.Products;
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            var db = new FabricsEntities();
+            var data = new Product()
+            {
+                ProductName = "White Cat",
+                Active = true,
+                Price = 199,
+                Stock = 5
+            };
+            db.Products.Add(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var db = new FabricsEntities();
+            var data = db.Products.Find(id);
+            db.Products.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new FabricsEntities();
+            var data = db.Products.Find(id);
+            return View(data);
+        }
+
+        public ActionResult Update(int id)
+        {
+            var db = new FabricsEntities();
+            var data = db.Products.Find(id);
+            data.ProductName += "!";
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Add20Percent()
+        {
+            var db = new FabricsEntities();
+            foreach (var item in db.Products)
+            {
+                if (item.Price.HasValue)
+                {
+                    item.Price = item.Price.Value * 1.2m;
+                }
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
